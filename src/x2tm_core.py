630ee0e721d801b3ce85cf4aff7763d82993d3a9
Krit Patrix <kzt.patrix@gmail.com>
2020-07-02 03:17:42 +0700

    :bug: Now remove only strings end with .0

--- src/x2tm_core.py
+++ src/x2tm_core.py
@@ -2,7 +2,8 @@
 
 import xlrd
 
-def phase_excel(exc, tmp):
+
+def parse_excel(exc, tmp):
     excf = xlrd.open_workbook(exc)
     sheet = excf.sheet_by_index(0)
 
@@ -11,13 +12,11 @@ def phase_excel(exc, tmp):
     for i in range(1, sheet.nrows):
         s = tmp
         for j in range(sheet.ncols):
+            rp = str(sheet.cell_value(i, j))
+            if rp.endswith(".0"):
+                rp = rp[:len(rp)-2]
             s = s.replace(
-                "[{}]".format(str(sheet.cell_value(0, j))), str(sheet.cell_value(i, j))
+                f"[{str(sheet.cell_value(0, j))}]", rp
             )
         out += s + "\n"
-
-    out = out.replace(".0", "")
     return out
-
-
-
